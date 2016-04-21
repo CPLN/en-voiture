6c8bb0d318dfb1c17b5fc5fff2e613f41c8a9908
LucienPe <lucien.perregaux@gmail.com>
2016-04-21 11:00:39 +0200

    Added Orientation in the Way class.

---
+++ EnVoiture/Way.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnVoiture
+{
+    class Way
+    {
+
+        public List<Orientation> Orientations
+        {
+            get;
+            private set;
+        }
+        public Way(List<Orientation> orientations)
+        {
+            this.Orientations = orientations;
+        }
+
+        public bool canGo(Orientation orientation)
+        {
+            bool can = false;
+            if (Orientations.Contains(orientation))
+            {
+                can = true;
+            }
+            return can;
+        }
+    }
+}
