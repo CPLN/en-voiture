e2c2b9051d02ddecc22cfa7fa76dc5ad78e15b7b
VincentGremion <Vincent.Gremion@rpn.ch>
2016-04-21 10:59:34 +0200

    Création de la classe PietonWidget

---
+++ EnVoiture/PietonWidget.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnVoiture
+{
+    class PietonWidget : RoadUserWidget
+    {
+        public override void Paint(Graphics g)
+        {
+            g.FillEllipse(Brushes.Azure);
+        }
+        private pieton p1;
+
+        public Pieton Pieton { get; private set; }
+
+        public PietonWidget(int x,int y,int width, int height)
+        {
+            this.pieton = new Pieton(x, y, width, height);
+        }
+
+    }
+}
