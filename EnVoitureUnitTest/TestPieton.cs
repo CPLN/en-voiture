3e04375e194e950e8a28951d71ccf7f8d2fbcb92
Serafal <Joaosilvalves@outlook.fr>
2016-04-21 10:50:20 +0200

    Creation classe de Test - TestPieton.cs

---
+++ EnVoitureUnitTest/TestPieton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using EnVoiture;
+
+namespace EnVoitureUnitTest
+{
+    [TestClass]
+    public class TestPieton
+    {
+        [TestMethod]
+        public void TestCreatePieton()
+        {
+            
+        }
+    }
+}
