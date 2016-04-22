d7ecab8c9b386b2106742ba184fc88e335aea419
nutr4x <nutr4x@gmail.com>
2016-04-22 13:40:51 +0200

    Création de la route grâce au paint + ajout de la route w1 dans la liste de route ways

--- EnVoiture/EnVoitureForm.cs
+++ EnVoiture/EnVoitureForm.cs
@@ -11,7 +11,7 @@ namespace EnVoiture
     {
         private List<RoadUserWidget> roadUsers;
         /// <summary>
-        /// Faire un foreach pour l'affichage
+        /// 
         /// </summary>
         private List<Way> Ways;
 
@@ -26,18 +26,23 @@ namespace EnVoiture
             roadUsers.Add(new CarWidget(10, 10, 30, 20));
 
             this.Ways = new List<Way>();
-          Way w1 = new Way(10, 10, 10, 10, new List<Orientation>() { Orientation.NORTH, Orientation.SOUTH});
+          
+            Way w1 = new Way(100, 100, 75, 150, new List<Orientation>() { Orientation.NORTH, Orientation.SOUTH});
+            Ways.Add(w1);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void EnVoiture_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
 
-            // route
-
-            g.FillRectangle(Brushes.Gray, 0, 0, 10, 10);
-
-            
+            foreach (Way ways in Ways)
+            {
+                ways.Paint(g);
+            }
 
             foreach (RoadUserWidget user in roadUsers)
             {
