558d71f6ffe9cd89490a1440551c85394a5ec855
LucienPe <lucien.perregaux@gmail.com>
2016-04-22 13:18:40 +0200

    Modified constructor in the TestSize method

--- EnVoitureUnitTest/TestWay.cs
+++ EnVoitureUnitTest/TestWay.cs
@@ -21,7 +21,7 @@ namespace EnVoitureUnitTest
         public void TestSize()
         {
             List<Orientation> o = new List<Orientation>();
-            Way w = new Way(0, 0, 20, 30, o);
+            Way w = new Way(new Point(0,0), new Size(20, 30), o);
             Assert.AreEqual(w.Size, new Size(20, 30));
         }
     }
