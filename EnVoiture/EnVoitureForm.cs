ae73a5d01d424b2efb387832aa27c282d79abe3f
Schneeberger Valentin <SchneebergVa@s2.rpn.ch>
2016-04-22 16:08:18 +0200

    creation d'un ligne noir

--- EnVoiture/EnVoitureForm.cs
+++ EnVoiture/EnVoitureForm.cs
@@ -23,8 +23,8 @@ namespace EnVoiture
             InitializeComponent();
 
             this.roadUsers = new List<RoadUserWidget>();
-
             this.Ways = new List<Way>();
+            Ways.Add(new Way(10,10,100,100,new List<Orientation>()));
             
         }
         /// <summary>
@@ -38,9 +38,12 @@ namespace EnVoiture
 
             foreach (Way way in Ways)
             {
+                Pen BlackPen = new Pen(Color.Black, 20);
+                Point point1 = new Point((way.Location.X+way.Size.Width)/2, way.Location.X);
+                Point point2 = new Point((way.Location.Y+way.Size.Width)/2,way.Location.Y+way.Size.Height);
                 way.Paint(g);
+                g.DrawLine(BlackPen, point1, point2);
             }
-
             foreach (RoadUserWidget user in roadUsers)
             {
                 user.Paint(g);
