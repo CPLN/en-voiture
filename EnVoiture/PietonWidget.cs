5ef1eee80a3fb806cb511f7ccf99c664b7201cd7
VincentGremion <Vincent.Gremion@rpn.ch>
2016-04-22 13:40:13 +0200

    Affichage du pieton

--- EnVoiture/PietonWidget.cs
+++ EnVoiture/PietonWidget.cs
@@ -14,9 +14,9 @@ namespace EnVoiture
 
         public Pieton Pieton { get; private set; }
 
-        public PietonWidget(Rectangle rectangle)
+        public PietonWidget(Rectangle rectangle, double v,double vmax)
         {
-            this.Pieton = new Pieton(rectangle);
+            this.Pieton = new Pieton(rectangle,v,vmax);
         }
 
         public PietonWidget(int x,int y,int width, int height, double v, double vmax)
@@ -25,7 +25,8 @@ namespace EnVoiture
         }
         public override void Paint(Graphics g)
         {
-            g.FillEllipse(Brushes.Azure, Pieton.Bounds);
+            g.FillEllipse(Brushes.Black, Pieton.Bounds);
         }
+        
     }
 }
