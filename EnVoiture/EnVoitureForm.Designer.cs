d52c3c4cf5900264b77bbb459a670a76af5a5ba6
LucienPe <lucien.perregaux@gmail.com>
2016-04-22 15:19:17 +0200

    Modified design

--- EnVoiture/EnVoitureForm.Designer.cs
+++ EnVoiture/EnVoitureForm.Designer.cs
@@ -30,10 +30,13 @@
         {
             this.pToolsBox = new System.Windows.Forms.Panel();
             this.pLevelDesigner = new System.Windows.Forms.Panel();
+            this.lblToolsBox = new System.Windows.Forms.Label();
+            this.pToolsBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // pToolsBox
             // 
+            this.pToolsBox.Controls.Add(this.lblToolsBox);
             this.pToolsBox.Location = new System.Drawing.Point(0, 0);
             this.pToolsBox.Name = "pToolsBox";
             this.pToolsBox.Size = new System.Drawing.Size(200, 540);
@@ -46,6 +49,16 @@
             this.pLevelDesigner.Size = new System.Drawing.Size(585, 540);
             this.pLevelDesigner.TabIndex = 1;
             // 
+            // lblToolsBox
+            // 
+            this.lblToolsBox.AutoSize = true;
+            this.lblToolsBox.Font = new System.Drawing.Font("Segoe Marker", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblToolsBox.Location = new System.Drawing.Point(12, 9);
+            this.lblToolsBox.Name = "lblToolsBox";
+            this.lblToolsBox.Size = new System.Drawing.Size(109, 24);
+            this.lblToolsBox.TabIndex = 0;
+            this.lblToolsBox.Text = "Boîte à outils";
+            // 
             // EnVoitureForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -56,6 +69,8 @@
             this.Name = "EnVoitureForm";
             this.Text = "En Voiture !";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.EnVoiture_Paint);
+            this.pToolsBox.ResumeLayout(false);
+            this.pToolsBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -64,6 +79,7 @@
 
         private System.Windows.Forms.Panel pToolsBox;
         private System.Windows.Forms.Panel pLevelDesigner;
+        private System.Windows.Forms.Label lblToolsBox;
     }
 }
 
