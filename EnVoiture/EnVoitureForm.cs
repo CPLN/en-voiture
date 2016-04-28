9820c00b5b50e1e81d2532c7fa2f9412d8b82558
Schneeberger Valentin <SchneebergVa@s2.rpn.ch>
2016-04-28 10:01:34 +0200

    ajout de propriété concernant la taille en x,y et la localisation x,y

--- EnVoiture/EnVoitureForm.cs
+++ EnVoiture/EnVoitureForm.cs
@@ -40,31 +40,36 @@ namespace EnVoiture
         private void EnVoiture_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
+
             foreach (Way way in Ways)
             {
+                way.Left = way.Location.X * 10;
+                way.Top = way.Location.Y * 10;
+                way.Size.Width = way.Size.Width * 100;
+                way.Size.Height = way.Size.Height * 100;
                 Pen BlackPen = new Pen(Color.Black, 20);
                 Point point2 = new Point(way.Location.X + way.Size.Width / 2, way.Location.Y + way.Size.Height/2);
                 Point point1;
                 way.Paint(g);
-                if(way.getDictionnaire.North)
+                if (way.GetDictionaire.ContainsKey(Orientation.NORTH) && way.GetDictionaire[Orientation.NORTH])
                 {
-                 point1 = new Point(way.Location.X + way.Size.Width / 2, way.Location.X);
-                 g.DrawLine(BlackPen, point1, point2);
+                    point1 = new Point(way.Location.X + way.Size.Width / 2, way.Location.X);
+                    g.DrawLine(BlackPen, point1, point2);
 
                 }
-                if(way.getDictionnaire.South)
+                if (way.GetDictionaire.ContainsKey(Orientation.SOUTH) && way.GetDictionaire[Orientation.SOUTH])
                 {
-                    point1 = new Point(way.Location.X + way.Size.Width / 2, way.Location.Y+way.Size.Height);
+                    point1 = new Point(way.Location.X + way.Size.Width / 2, way.Location.Y + way.Size.Height);
                     g.DrawLine(BlackPen, point1, point2);
                 }
-                if(way.getDictionnaire.East)
+                if (way.GetDictionaire.ContainsKey(Orientation.EAST) && way.GetDictionaire[Orientation.EAST])
                 {
-                    point1 = new Point(way.Location.X, way.Location.Y + way.Size.Height/2);
+                    point1 = new Point(way.Location.X, way.Location.Y + way.Size.Height / 2);
                     g.DrawLine(BlackPen, point1, point2);
                 }
-                if(way.getDictionnaire.West)
+                if (way.GetDictionaire.ContainsKey(Orientation.WEST) && way.GetDictionaire[Orientation.WEST])
                 {
-                    point1 = new Point(way.Location.X+way.Size.Width, way.Location.Y + way.Size.Height / 2);
+                    point1 = new Point(way.Location.X + way.Size.Width, way.Location.Y + way.Size.Height / 2);
                     g.DrawLine(BlackPen, point1, point2);
                 }
             }
--- EnVoiture/Way.cs
+++ EnVoiture/Way.cs
@@ -21,6 +21,29 @@ namespace EnVoiture
             private set;
         }
 
+        public int Left
+        {
+            get
+            {
+                return Location.X;
+            }
+            set
+            {
+                Location = new Point(value, Location.Y);
+            }
+        }
+        public int Top
+        {
+            get
+            {
+                return Location.Y;
+            }
+            set
+            {
+                Location = new Point(Location.X, value);
+            }
+        }
+        
         /// <summary>
         /// Size of the way
         /// </summary>
@@ -28,6 +51,30 @@ namespace EnVoiture
         {
             get;
             private set;
+        }
+
+        public int TailleX
+        {
+            get
+            {
+                return Size.Width;
+            }
+            set
+            {
+                Size = new Size(value, Size.Height);
+            }
+
+        }
+        public int TailleY
+        {
+            get
+            {
+                return Size.Height;
+            }
+            set
+            {
+                Size = new Size(Size.Width, value);
+            }
         }
 
         /// <summary>
@@ -84,17 +131,10 @@ namespace EnVoiture
         /// <param name="point"></param>
         /// <param name="size"></param>
         /// <param name="orientations"></param>
-        public Way(Point point, System.Drawing.Size size, List<Orientation> orientations)
-        {
-            // TODO: Complete member initialization
-            this.point = point;
-            this.Size = size;
-            this.Orientations = orientations;
-        }
         /// <summary>
         /// Get sur Dictionaire
         /// </summary>
-        public Dictionary<Orientation, bool> GetDictionaire { get { return _orientsWays; } private set; }
+        public Dictionary<Orientation, bool> GetDictionaire { get { return _orientsWays; }}
 
         /// <summary>
         /// Création de liste de Ways selon les paramètres entrés. Chaque Way fait 25cm2
@@ -107,7 +147,6 @@ namespace EnVoiture
             int nbWays = largeurVille * hauteurVille;
             List<Way> _waysVille = new List<Way>();
             Dictionary<Orientation, bool> _orientsWays = new Dictionary<Orientation,bool>();
-
             for (int i = 1; i <= largeurVille; i++)
 			{
                 for (int j = 0; j < hauteurVille; j++)
