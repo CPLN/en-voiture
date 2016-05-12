10935728ad77ce23f801913d7a41dd860aa2499f
Schneeberger Valentin <SchneebergVa@s2.rpn.ch>
2016-05-12 10:36:08 +0200

    refactoring et création du deuzieme test de l'accélération

--- EnVoitureUnitTest/testAcceleration.cs
+++ EnVoitureUnitTest/testAcceleration.cs
@@ -9,30 +9,62 @@ namespace EnVoitureUnitTest
     public class testAcceleration
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAcceleration1()
         {
             Car car = new Car(new Rectangle(0, 0, 0, 0), 5);
 
             Assert.AreEqual(0, car.Vitesse);
             car.Avancer();
+
             Assert.AreEqual(1, car.Vitesse);
             car.Avancer();
+
             Assert.AreEqual(2, car.Vitesse);
             car.Avancer();
+
             Assert.AreEqual(3, car.Vitesse);
+            car.Avancer();
+
+            Assert.AreEqual(4, car.Vitesse);
+            car.Avancer();
+
+            Assert.AreEqual(5, car.Vitesse);
 
             car.Avancer();
+            Assert.AreEqual(5, car.Vitesse);
+
+
+            car.Ralentir();
             Assert.AreEqual(4, car.Vitesse);
 
-            car.Avancer();
-            Assert.AreEqual(5, car.Vitesse);
-            car.Avancer();
-            Assert.AreEqual(5, car.Vitesse);
+            car.Ralentir();
+            Assert.AreEqual(3, car.Vitesse);
 
-            car.Reculer();
-            car.Reculer();
-            car.Reculer();
+            car.Ralentir();
+            Assert.AreEqual(2, car.Vitesse);
 
+            car.Ralentir();
+            Assert.AreEqual(1, car.Vitesse);
+
+            car.Ralentir();
+            Assert.AreEqual(0, car.Vitesse);
+        }
+
+        [TestMethod]
+        public void TestAcceleration2()
+        {
+            Car car = new Car(new Rectangle(0, 0, 0, 0), 5);
+
+            car.Avancer();
+            car.Avancer();
+            car.Avancer();
+            car.Avancer();
+            car.Avancer();
+
+            car.Freiner();
+            Assert.AreEqual(2, car.Vitesse);
+
+            car.Freiner();
             Assert.AreEqual(0, car.Vitesse);
         }
     }
