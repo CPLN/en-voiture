bbb0c179210cf6853e9f687839bee52453d6d06f
nutr4x <nutr4x@gmail.com>
2016-05-13 15:31:48 +0200

    création des classes de base et les méthodes pour l'orientation de la route

--- EnVoiture/Vue/GenerateurWidget.cs
+++ EnVoiture/Vue/GenerateurWidget.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using EnVoiture.Modele;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EnVoiture.Vue
 {
+   
     class GenerateurWidget
     {
+        const int HAUTEUR = 200;
+        const int LARGEUR = 200;
+        public GenerateurWidget()
+        {
+            Generateur g = new Generateur();
+        }
+        /// <summary>
+        /// Cette fonction reçoit en paramètre un point et permet de le transformer en orientation.
+        /// </summary>
+        /// <param name="point"></param>
+        public void DetectionOrientation(Point point){}
+
+        
     }
 }
