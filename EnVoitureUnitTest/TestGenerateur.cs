150e8ecd982ba74011f90abf44e3833e7b4cd2ba
LucienPe <lucien.perregaux@gmail.com>
2016-05-19 10:35:45 +0200

    Ajout des unittests sud et ouest

--- EnVoitureUnitTest/TestGenerateur.cs
+++ EnVoitureUnitTest/TestGenerateur.cs
@@ -106,20 +106,42 @@ namespace EnVoitureUnitTest
          */
 
         /// <summary>
-        /// Test si l'état du sud n'a pas changé
+        /// Test si l'état de l'est a changé de rien à route simple
         /// </summary>
         [TestMethod]
-        public void TestEditionRouteSouth()
+        public void TestEditionRouteSudRienRoute()
         {
             Orientation orientation = Orientation.SOUTH;
-            Route route = new Route(0, 0, 1, 1, new List<Orientation>());
-            Generateur generateur = new Generateur(route);
+            Generateur generateur = new Generateur(new Route(0, 0, 1, 1, null));
             generateur.EditionRoute(orientation);
 
-            bool obstacle = false;
-            if (route.GetDictionaire.ContainsKey(orientation))
-                obstacle = route.GetDictionaire[orientation];
-            Assert.AreEqual(obstacle, false);
+            Assert.AreEqual(generateur.Route.GetDictionaire[orientation], Obstacle.ROUTE);
+        }
+
+        /// <summary>
+        /// Test si l'état de l'est a changé de route simple à route avec trottoir
+        /// </summary>
+        [TestMethod]
+        public void TestEditionRouteSudRouteTrottoir()
+        {
+            Orientation orientation = Orientation.SOUTH;
+            Generateur generateur = new Generateur(new Route(0, 0, 1, 1, null));
+            generateur.EditionRoute(orientation);
+
+            Assert.AreEqual(generateur.Route.GetDictionaire[orientation], Obstacle.ROUTETROTTOIR);
+        }
+
+        /// <summary>
+        /// Test si l'état de l'est a changé de route avec trottoir à rien
+        /// </summary>
+        [TestMethod]
+        public void TestEditionRouteSudTrottoirRien()
+        {
+            Orientation orientation = Orientation.SOUTH;
+            Generateur generateur = new Generateur(new Route(0, 0, 1, 1, null));
+            generateur.EditionRoute(orientation);
+
+            Assert.AreEqual(generateur.Route.GetDictionaire[orientation], Obstacle.RIEN);
         }
 
         /*
@@ -129,20 +151,42 @@ namespace EnVoitureUnitTest
          */
 
         /// <summary>
-        /// Test si l'état de l'ouest n'a pas changé
+        /// Test si l'état de l'est a changé de rien à route simple
         /// </summary>
         [TestMethod]
-        public void TestEditionRouteWest()
+        public void TestEditionRouteOuestRienRoute()
         {
             Orientation orientation = Orientation.WEST;
-            Route route = new Route(0, 0, 1, 1, new List<Orientation>());
-            Generateur generateur = new Generateur(route);
+            Generateur generateur = new Generateur(new Route(0, 0, 1, 1, null));
             generateur.EditionRoute(orientation);
 
-            bool obstacle = false;
-            if (route.GetDictionaire.ContainsKey(orientation))
-                obstacle = route.GetDictionaire[orientation];
-            Assert.AreEqual(obstacle, false);
+            Assert.AreEqual(generateur.Route.GetDictionaire[orientation], Obstacle.ROUTE);
+        }
+
+        /// <summary>
+        /// Test si l'état de l'est a changé de route simple à route avec trottoir
+        /// </summary>
+        [TestMethod]
+        public void TestEditionRouteOuestRouteTrottoir()
+        {
+            Orientation orientation = Orientation.WEST;
+            Generateur generateur = new Generateur(new Route(0, 0, 1, 1, null));
+            generateur.EditionRoute(orientation);
+
+            Assert.AreEqual(generateur.Route.GetDictionaire[orientation], Obstacle.ROUTETROTTOIR);
+        }
+
+        /// <summary>
+        /// Test si l'état de l'est a changé de route avec trottoir à rien
+        /// </summary>
+        [TestMethod]
+        public void TestEditionRouteOuestTrottoirRien()
+        {
+            Orientation orientation = Orientation.WEST;
+            Generateur generateur = new Generateur(new Route(0, 0, 1, 1, null));
+            generateur.EditionRoute(orientation);
+
+            Assert.AreEqual(generateur.Route.GetDictionaire[orientation], Obstacle.RIEN);
         }
     }
 }
