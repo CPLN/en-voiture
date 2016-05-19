0548a67ada86de98d3fa75b4d8aca9f78a07a362
LucienPe <lucien.perregaux@gmail.com>
2016-05-19 09:27:11 +0200

    Ajout des unittests TestEditionRoute avec l'orientation nord, est, sud et ouest.

--- EnVoitureUnitTest/TestGenerateur.cs
+++ EnVoitureUnitTest/TestGenerateur.cs
@@ -10,13 +10,59 @@ namespace EnVoitureUnitTest
     public class TestGenerateur
     {
         [TestMethod]
-        public void TestEditionRoute()
+        public void TestEditionRouteNorth()
         {
+            Orientation orientation = Orientation.NORTH;
             Route route = new Route(0, 0, 1, 1, new List<Orientation>());
             Generateur generateur = new Generateur(route);
-            generateur.EditionRoute(Orientation.NORTH);
+            generateur.EditionRoute(orientation);
 
-            Assert.AreEqual(route.GetDictionaire[Orientation.NORTH], false);
+            bool obstacle = false;
+            if (route.GetDictionaire.ContainsKey(orientation))
+                obstacle = route.GetDictionaire[orientation];
+            Assert.AreEqual(obstacle, false);
+        }
+
+        [TestMethod]
+        public void TestEditionRouteEast()
+        {
+            Orientation orientation = Orientation.EAST;
+            Route route = new Route(0, 0, 1, 1, new List<Orientation>());
+            Generateur generateur = new Generateur(route);
+            generateur.EditionRoute(orientation);
+
+            bool obstacle = false;
+            if (route.GetDictionaire.ContainsKey(orientation))
+                obstacle = route.GetDictionaire[orientation];
+            Assert.AreEqual(obstacle, false);
+        }
+
+        [TestMethod]
+        public void TestEditionRouteSouth()
+        {
+            Orientation orientation = Orientation.SOUTH;
+            Route route = new Route(0, 0, 1, 1, new List<Orientation>());
+            Generateur generateur = new Generateur(route);
+            generateur.EditionRoute(orientation);
+
+            bool obstacle = false;
+            if (route.GetDictionaire.ContainsKey(orientation))
+                obstacle = route.GetDictionaire[orientation];
+            Assert.AreEqual(obstacle, false);
+        }
+
+        [TestMethod]
+        public void TestEditionRouteWest()
+        {
+            Orientation orientation = Orientation.WEST;
+            Route route = new Route(0, 0, 1, 1, new List<Orientation>());
+            Generateur generateur = new Generateur(route);
+            generateur.EditionRoute(orientation);
+
+            bool obstacle = false;
+            if (route.GetDictionaire.ContainsKey(orientation))
+                obstacle = route.GetDictionaire[orientation];
+            Assert.AreEqual(obstacle, false);
         }
     }
 }
