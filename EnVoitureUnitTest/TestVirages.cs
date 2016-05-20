2fe40f1998577270727049ddb7f793ba310d50cd
Serafal <Joaosilvalves@outlook.fr>
2016-05-20 14:30:24 +0200

    Création de classe de Test (TestVirages) dans EnVoitureUnitTest

---
+++ EnVoitureUnitTest/TestVirages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using EnVoiture;
+
+namespace EnVoitureUnitTest
+{
+    [TestClass]
+    public class TestVirages
+    {
+
+    }
+}
