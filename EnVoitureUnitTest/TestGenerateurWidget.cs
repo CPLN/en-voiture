ad124db7dae798aea31e1b166570a81286526317
LucienPe <lucien.perregaux@gmail.com>
2016-05-20 14:06:12 +0200

    Ajout des unittests DetectionOrientation ouest.

---
+++ EnVoitureUnitTest/TestGenerateurWidget.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EnVoiture;
+using EnVoiture.Vue;
+using System.Drawing;
+
+namespace EnVoitureUnitTest
+{
+    [TestClass]
+    public class TestGenerateurWidget
+    {
+        /*
+         * 
+         * Tests ouest
+         * 
+         */
+
+        /// <summary>
+        /// Test si on clique en x50 et y50 que cela retourne ouest
+        /// </summary>
+        [TestMethod]
+        public void TestDetectionOrientationOuestX13Y24()
+        {
+            GenerateurWidget generateurw = new GenerateurWidget();
+
+            Orientation o = generateurw.DetectionOrientation(new Point(13, 24));
+            Assert.AreEqual(o, Orientation.OUEST);
+        }
+
+        /// <summary>
+        /// Test si on clique en x0 et y0 que cela retourne ouest
+        /// </summary>
+        [TestMethod]
+        public void TestDetectionOrientationOuestX0Y0()
+        {
+            GenerateurWidget generateurw = new GenerateurWidget();
+
+            Orientation o = generateurw.DetectionOrientation(new Point(0, 0));
+            Assert.AreEqual(o, Orientation.OUEST);
+        }
+
+        /// <summary>
+        /// Test si on clique en x0 et y1 que cela retourne ouest
+        /// </summary>
+        [TestMethod]
+        public void TestDetectionOrientationOuestX0Y1()
+        {
+            GenerateurWidget generateurw = new GenerateurWidget();
+
+            Orientation o = generateurw.DetectionOrientation(new Point(0, 1));
+            Assert.AreEqual(o, Orientation.OUEST);
+        }
+
+        /// <summary>
+        /// Test si on clique en x1 et y1 que cela retourne ouest
+        /// </summary>
+        [TestMethod]
+        public void TestDetectionOrientationOuestX1Y1()
+        {
+            GenerateurWidget generateurw = new GenerateurWidget();
+
+            Orientation o = generateurw.DetectionOrientation(new Point(1, 1));
+            Assert.AreEqual(o, Orientation.OUEST);
+        }
+
+        /// <summary>
+        /// Test si on clique en x50 et y50 que cela retourne ouest
+        /// </summary>
+        [TestMethod]
+        public void TestDetectionOrientationOuestX50Y50()
+        {
+            GenerateurWidget generateurw = new GenerateurWidget();
+
+            Orientation o = generateurw.DetectionOrientation(new Point(50, 50));
+            Assert.AreEqual(o, Orientation.OUEST);
+        }
+    }
+}
