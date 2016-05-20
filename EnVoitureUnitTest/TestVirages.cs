8cdf111c84bf89584d2e74cdf5bb23b47f1d0b13
Serafal <Joaosilvalves@outlook.fr>
2016-05-20 14:41:05 +0200

    Delete fonctions inutiles dans Route.cs Création méthode de test

--- EnVoitureUnitTest/TestVirages.cs
+++ EnVoitureUnitTest/TestVirages.cs
@@ -2,12 +2,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Drawing;
 using EnVoiture;
+using System.Collections.Generic;
 
 namespace EnVoitureUnitTest
 {
+
     [TestClass]
     public class TestVirages
     {
+        private Voiture voiture;
+        private List<RoadUserWidget> roadUsers;
+        private bool bAvancer = false;
+        private bool bReculer = false;
+        private bool bDroite = false;
+        private bool bGauche = false;
 
+        [TestMethod]
+        public class TestRotation
+        {
+
+        }
     }
 }
